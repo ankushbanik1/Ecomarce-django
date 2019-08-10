2d74a4f087d923e8b71116df6bdbe89275da7239
ankush banik <ankushbanik123@gmail.com>
2019-08-11 00:33:01 +0530

    tracker,checkout

--- ecomarce/shop/models.py
+++ ecomarce/shop/models.py
@@ -15,6 +15,9 @@ class Product(models.Model):
         return self.product_name
 
 
+        
+
+
 class Contact(models.Model):
     contact_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
@@ -26,3 +29,33 @@ class Contact(models.Model):
         return self.name 
      
 
+class Orders(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    items_json = models.CharField(max_length=5000)
+    name = models.CharField(max_length=90)
+    email = models.CharField(max_length=111)
+    address = models.CharField(max_length=111)
+    city = models.CharField(max_length=111)
+    state = models.CharField(max_length=111)
+    zip_code = models.CharField(max_length=111)
+    phone = models.CharField(max_length=111, default="")
+    
+
+
+    def __str__(self):
+        return self.name 
+
+
+class orderUpdate(models.Model):
+    update_id  = models.AutoField(primary_key=True)
+    order_id = models.IntegerField(default="")
+    update_desc = models.CharField(max_length=5000)
+    timestamp =models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.order_id
+
+
+
+
+    
--- ecomarce/shop/views.py
+++ ecomarce/shop/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from . models import Product,Contact
+from . models import Product,Contact,Orders
+from . models import orderUpdate
+import json
 from math import ceil
 # Create your views here.
 def product(request):
@@ -43,14 +45,51 @@ def about(request):
     return render(request, 'shop/about.html')
         
 def checkout(request):
+    if request.method=="POST":
+        items_json = request.POST.get('itemsJson', '')
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        address = request.POST.get('address1', '') + " " + request.POST.get('address2', '')
+        city = request.POST.get('city', '')
+        state = request.POST.get('state', '')
+        zip_code = request.POST.get('zip_code', '')
+        phone = request.POST.get('phone', '')
+        order = Orders(items_json=items_json, name=name, email=email, address=address, city=city,
+                       state=state, zip_code=zip_code, phone=phone)
+        order.save()
+        
+        update=orderUpdate(order_id= order.order_id,update_desc="The order has been placed")
+        update.save()
+        thank = True
+        id = order.order_id
+        return render(request, 'shop/checkout.html',{'thank':thank,'id':id})
+
+        
     return render(request, 'shop/checkout.html')
         
-def traking(request):
-    return render(request, 'shop/traking.html')
-# def index(request):
-#     objects = Product.objects.all()
-#     context = {
-#             'objects' : objects
-#             }
-#     return render(request,'shop/index.html',context)
+def tracker(request):
+    if request.method=="POST":
+        orderId = request.POST.get('orderId', '')
+        email = request.POST.get('email', '')
+        try:
+            order = Orders.objects.filter(order_id=orderId, email=email)
+            if len(order)>0:
+                update = orderUpdate.objects.filter(order_id=orderId)
+                updates = []
+                for item in update:
+                    updates.append({'text': item.update_desc, 'time': item.timestamp})
+                    response = json.dumps(updates, default=str)
+                return HttpResponse(response)
+            else:
+                return HttpResponse('{}')
+        except Exception as e:
+            return HttpResponse('{}')
+
+    return render(request, 'shop/tracker.html')
+
+
+        
+
     
+   
+
